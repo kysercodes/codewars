1bb9d6dec99e91045a370ae0b857733da9621d98
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-06-18 11:54:18 -0400

    added new solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -18,3 +18,26 @@ return str.replace(/[_-]/g, " ").split(" ").map((word,idx) => {
 }
 
 console.log(toCamelCase("The_Stealth-Warrior"))
+
+// Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+
+// Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+
+// If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+
+// Examples
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+
+function order(words){
+  // ...
+   const newWord =  words.split(" ");
+   
+   return newWord.sort((a,b) => {
+     let aDD = a.match(/\d/);
+     let bDD =b.match(/\d/);
+     
+     return aDD - bDD
+   })
+}
+
+console.log(order("4of Fo1r pe6ople g3ood th5e the2"))
