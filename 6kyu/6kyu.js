567530e5f17b48fca02911ed35b294b23339199e
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-06-22 13:06:27 -0400

    added new solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -41,3 +41,25 @@ function order(words){
 }
 
 console.log(order("4of Fo1r pe6ople g3ood th5e the2"))
+
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+
+function solution(str){
+  
+  let pairs = []
+  if (str.length % 2 !== 0) {
+    str += '_';
+  }
+   if(str.length % 2 === 0) {
+    for (let i = 0; i < str.length; i += 2) {
+  
+    pairs.push(str.slice(i, i + 2 ));
+  }
+   } 
+  return pairs
+}
