0640d5ae5627659840963fd09a9388f5d4b3836a
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-06-23 10:32:44 -0400

    added solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -63,3 +63,27 @@ function solution(str){
    } 
   return pairs
 }
+
+// In this simple Kata your task is to create a function that turns a 
+// string into a Mexican Wave. You will be passed a string and you must 
+// return that string in an array where an uppercase letter is a person 
+// standing up. 
+//  1.  The input string will always be lower case but maybe empty.
+
+//  2.  If the character in the string is whitespace then pass over it as
+  // if it was an empty seat
+
+  // wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+  function wave(str){
+  let newArr = []
+  // Code here
+  for(let i = 0; i < str.length; i++) {
+    if (str[i] === ' ') {
+      continue;
+    }
+   let waveStr = str.slice(0, i) + str[i].toUpperCase() + str.slice(i + 1);
+    newArr.push(waveStr)
+  }
+return newArr
+}
+console.log(wave("hello"))
