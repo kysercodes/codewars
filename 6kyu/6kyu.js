acde5cd81c090071b07dc10ce013beb4c6eb1830
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-06-24 12:54:44 -0400

    added new solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -87,3 +87,67 @@ function solution(str){
 return newArr
 }
 console.log(wave("hello"))
+
+// A string is considered to be in title case if each word in the string is either 
+// (a) capitalised (that is, only the first letter of the word is in upper case) or
+//  (b) considered to be an exception and put entirely into lower case unless it is the 
+//  first word, which is always capitalised.
+
+// Write a function that will convert a string into title case, given an 
+// optional list of exceptions (minor words). The list of minor words will be given as 
+// a string with each word separated by a space. Your function should ignore the case of
+//  the minor words string -- it should behave in the same way even if the case of the 
+//  minor word string is changed.
+
+// Arguments (Other languages)
+// First argument (required): the original string to be converted.
+// Second argument (optional): space-delimited list of minor words that 
+// must always be lowercase except for the first word in the string. The 
+// JavaScript/CoffeeScript tests will pass undefined when this argument is unused.
+// Example
+// titleCase('a clash of KINGS', 'a an the of') // should return: 'A Clash of Kings'
+// titleCase('THE WIND IN THE WILLOWS', 'The In') // should return: 'The Wind in the Willows'
+// titleCase('the quick brown fox') // should return: 'The Quick Brown Fox'
+
+//  MY SOLUTION
+function titleCase(title, minorWords) {
+  if ( !minorWords) {
+   return title.split(" ").map((word) => {
+     return word.charAt(0).toUpperCase() + word.slice(1)
+   }).join(" ")
+ }
+  const minorArray = minorWords.toUpperCase().split(" ")
+  console.log(minorArray)
+ 
+ let newTitle = title.split(" ").slice(1)
+  newTitle =  newTitle.map((word) => {
+     if(minorArray.includes(word.toUpperCase())) {
+        
+       return word.toLowerCase()
+     } else {
+       return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()
+     }
+  })
+  
+ 
+  let capFirst = title.split(" ")[0].charAt(0).toUpperCase() + title.split(" ")[0].slice(1).toLowerCase()
+  
+   console.log(newTitle.unshift(capFirst))
+  return newTitle.join(" ")
+ 
+ 
+}
+
+
+// A correct solution
+function titleCase(title, minorWords) {
+  var minorWords = typeof minorWords !== "undefined" ? minorWords.toLowerCase().split(' ') : [];
+  return title.toLowerCase().split(' ').map(function(v, i) {
+    if(v != "" && ( (minorWords.indexOf(v) === -1) || i == 0)) {
+      v = v.split('');
+      v[0] = v[0].toUpperCase();
+      v = v.join('');
+    }
+    return v;
+  }).join(' ');
+}
