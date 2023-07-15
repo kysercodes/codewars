3cead6839d8902aad05648230bb12516e6bf85dd
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-07-15 13:48:07 -0400

    added 6kyu string

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -365,3 +365,24 @@ function findLongest(array){
 
 
 console.log(findLongest([8, 9000, 3000]))
+
+// 6kyu string
+
+
+/* 
+A pangram is a sentence that contains every single letter of the alphabet at 
+least once. For example, the sentence "The quick brown fox jumps over the lazy dog" 
+is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+Given a string, detect whether or not it is a pangram. Return True if it is, 
+False if not. Ignore numbers and punctuation.
+*/
+
+function isPangram(string) {
+  return 'abcdefghijklmnopqrstuvwxyz'
+    .split('')
+    .every((x) => string.toLowerCase().includes(x));
+}
+
+const string = "The quick brown fox jumps over the lazy dog.";
+console.log(isPangram(string));
