bf37d2b8ff95ea1b7088185fbfd402e1e8ebdce5
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-07-17 13:37:18 -0400

    updated description

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -389,7 +389,7 @@ console.log(isPangram(string));
 
 
 // Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
-
+//  8kkyu array
 // Example(Input => Output):
 // 35231 => [1,3,2,5,3]
 // 0 => [0] */
