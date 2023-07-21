05f527eaf99bd21aa4edc9b1d0b01906366afa51
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-07-21 13:31:44 -0400

    added 7kyu array problem max product

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -470,3 +470,35 @@ const gimme = function (inputArray) {
   });
   
   };
+
+
+
+//   Given an array of integers , Find the maximum product obtained from 
+//   multiplying 2 adjacent numbers in the array.
+
+// Notes
+// Array/list size is at least 2.
+
+// Array/list numbers could be a mixture of positives, negatives also zeroes .
+
+// Input >> Output Examples
+// adjacentElementsProduct([1, 2, 3]); ==> return 6
+
+function adjacentElementsProduct(array) {
+  // max product
+  // loop through the array
+  let newArr = []
+  for(let i=0; i < array.length-1; i++){
+    newArr.push(array[i]*array[i+1])
+  }  
+  return Math.max(...newArr)
+   
+
+  // add each element and the adjacent element
+  // push the next element to a new array
+  // finally find the largest number in the new array
+  
+  console.log(newArr)
+}
+
+console.log(adjacentElementsProduct([1, 2, 3,7]))
