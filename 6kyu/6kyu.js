e52849dd82651b616dc6a7f0936002d3947859c3
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-07-22 15:54:01 -0400

    added 7kyu array problem

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -502,3 +502,14 @@ function adjacentElementsProduct(array) {
 }
 
 console.log(adjacentElementsProduct([1, 2, 3,7]))
+
+
+// 7kyu array problem
+// You are given a dictionary/hash/object containing some languages and your test results in the given languages. Return the list of languages where your test score is at least 60, in descending order of the scores.
+
+// Note: the scores will always be unique (so no duplicate values)
+
+// Examples
+// {"Java": 10, "Ruby": 80, "Python": 65}    -->  ["Ruby", "Python"]
+// {"Hindi": 60, "Dutch" : 93, "Greek": 71}  -->  ["Dutch", "Greek", "Hindi"]
+// {"C++": 50, "ASM": 10, "Haskell": 20}     -->  []
