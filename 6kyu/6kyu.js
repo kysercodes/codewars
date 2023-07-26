5b00dad0845d8f0e623e44a21d83fd510548b1d4
Antoine <65553139+kysercodes@users.noreply.github.com>
2023-07-26 13:25:15 -0400

    added 6kyu string

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -589,3 +589,27 @@ function isSortedAndHow(array) {
     return "no";
   }
 }
+// 6kyu string problem
+// Complete the solution so that it splits the string into pairs of two characters. If the string contains an odd number of characters then it should replace the missing second character of the final pair with an underscore ('_').
+
+// Examples:
+
+// * 'abc' =>  ['ab', 'c_']
+// * 'abcdef' => ['ab', 'cd', 'ef']
+function solution(str){
+  str = str.split("")
+  
+  if(str.length % 2 === 1) {
+     str.push('_')
+  
+   }
+  const result = [];
+  for (let i = 0; i < str.length; i += 2) {
+    result.push(str[i] + str[i + 1]);
+  }
+  
+  return result;
+}
+
+
+console.log(solution("abcdef"))
