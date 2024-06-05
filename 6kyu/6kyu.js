d58d5af0d1758bda5949fbee4f3cad6d7435b741
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-06-05 12:39:00 -0400

    added 8kyu solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -683,3 +683,20 @@ for (let i = 0; i < input.length; i++) {
   }
   return newArr
 }
+
+// Create a function called shortcut to remove the lowercase vowels (a, e, i, o, u ) in a given string.
+
+// Examples
+// "hello"     -->  "hll"
+// "codewars"  -->  "cdwrs"
+// "goodbye"   -->  "gdby"
+// "HELLO"     -->  "HELLO"
+
+function shortcut (string) {
+  let vowelPattern = /[aeiou]/;
+  let result = string.replace(/[aeiou]/g, '');
+  return result;
+  
+}
+
+console.log(shortcut("how are you today?"))
