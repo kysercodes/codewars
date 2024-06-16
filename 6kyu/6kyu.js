efd2482a1c0911239ac218b7c94291c14cb0dd57
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-06-16 18:47:58 -0400

    added Find the stray number 7kyu

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -849,3 +849,29 @@ function sumMix(x){
  }
  return sum
 }
+
+// You are given an odd-length array of integers, in which all of them are the same, except for one single number.
+
+// Complete the method which accepts such an array, and returns that single different number.
+
+// The input array will always be valid! (odd-length >= 3)
+
+// Examples
+// [1, 1, 2] ==> 2
+// [17, 17, 3, 17, 17, 17, 17] ==> 3
+
+function stray(numbers) {
+  let repeatingNumber;
+   if (numbers[0] === numbers[1] ||numbers[0] === numbers[2]) {
+       repeatingNumber = numbers[0];
+   } else {
+       repeatingNumber = numbers[1];
+   }
+
+   // Use the find method to locate the number that is different
+   return numbers.find(num => num !== repeatingNumber);
+
+
+}
+
+stray([1, 1, 2])
