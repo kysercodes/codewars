e93f7c25d71d87523b92839a8b0ddb57156976b8
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-06-25 14:00:07 -0400

    added 8kyu solution Sum without highest and lowest number

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1027,3 +1027,24 @@ function mouthSize(animal) {
 }
 
 console.log(mouthSize("toucan"))
+
+// Sum all the numbers of a given array ( cq. list ), except the highest and the lowest element ( by value, not by index! ).
+
+// The highest or lowest element respectively is a single element at each edge, even if there are more than one with the same value.
+
+function sumArray(array) {
+  if (array == null) {
+    return 0;
+  } else if (array.length < 2) {
+    return 0;
+  } else {
+    array = array.sort(function(a,b) {return a - b;});
+    var total = 0;
+    for (var i = 1; i < array.length - 1; i++) {
+      total += array[i];
+    }
+    return total;
+  }
+}
+
+console.log( sumArray([ 6, 2, 1, 8, 10 ]))
