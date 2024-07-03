23ed167c50b8c226d1d91aea19e8f9e717d2bfaf
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-07-03 10:07:08 -0400

    added 7kyu soulution reverse string

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1145,3 +1145,25 @@ function replace(s){
 }
 
 console.log(replace("!Hi! Hi!"));
+
+// Given a string str, reverse it and omit all non-alphabetic characters.
+
+// Example
+// For str = "krishan", the output should be "nahsirk".
+
+// For str = "ultr53o?n", the output should be "nortlu".
+
+// Input/Output
+// [input] string str
+// A string consists of lowercase latin letters, digits and symbols.
+
+// [output] a string
+
+function reverseLetter(str) {
+  //coding and coding..
+  const newStr = str.replace(/[^a-zA-Z]/g,"")
+   return newStr.split("").reverse().join("")
+  
+}
+
+console.log(reverseLetter("ultr53o?n"))
