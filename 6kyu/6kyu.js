e019e32593dfe2b6b89c28a7628177eca5af55c1
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-07-09 11:59:57 -0400

    added 7kyu filter lists

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1167,3 +1167,21 @@ function reverseLetter(str) {
 }
 
 console.log(reverseLetter("ultr53o?n"))
+
+// In this kata you will create a function that takes a list of non-negative integers and strings and returns a new list with the strings filtered out.
+
+// Example
+// filter_list([1,2,'a','b']) == [1,2]
+// filter_list([1,'a','b',0,15]) == [1,0,15]
+// filter_list([1,2,'aasf','1','123',123]) == [1,2,123]
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+//   will there ever be negative numbers?
+//   will the array ever be empty?
+//   input array
+//   output array - non negative integers
+//   turn array into string 
+  return l.filter(item => typeof item === 'number' && item >= 0);
+}
+console.log(filter_list([1,2,'a','b']))
