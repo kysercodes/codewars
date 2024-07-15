95f52367a87acb90fed5b20e80fc30aed323884b
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-07-15 13:16:09 -0400

    added thid

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1185,3 +1185,45 @@ function filter_list(l) {
   return l.filter(item => typeof item === 'number' && item >= 0);
 }
 console.log(filter_list([1,2,'a','b']))
+
+
+// In a factory a printer prints labels for boxes. For one kind of boxes the printer has to use colors which, for the sake of simplicity, are named with letters from a to m.
+
+// The colors used by the printer are recorded in a control string. For example a "good" control string would be aaabbbbhaijjjm meaning that the printer used three times color a, four times color b, one time color h then one time color a...
+
+// Sometimes there are problems: lack of colors, technical malfunction and a "bad" control string is produced e.g. aaaxbbbbyyhwawiwjjjwwm with letters not from a to m.
+
+// You have to write a function printer_error which given a string will return the error rate of the printer as a string representing a rational whose numerator is the number of errors and the denominator the length of the control string. Don't reduce this fraction to a simpler expression.
+
+// The string has a length greater or equal to one and contains only letters from ato z.
+
+function printerError(s) {
+  const regex = /[^a-m]/g;
+    // your code
+  
+//   one kind of box uses colors  letters names a - m
+//   colors are recorded in a string
+  const length = s.length
+ const errors =  s.match(regex)
+ console.log(errors)
+
+  const errorsCount = errors ? errors.length : 0;
+
+ return  `${errorsCount}/${length}`
+}
+console.log(printerError("accwcwcwcw"))
+
+// Finish the solution so that it sorts the passed in array of numbers. If the function passes in an empty array or null/nil value then it should return an empty array.
+
+// For example:
+
+// solution([1, 2, 10, 50, 5]); // should return [1,2,5,10,50]
+// solution(null); // should return []
+
+function solution(nums){
+  if(nums === null) {
+    return []
+  }
+   
+   return nums.sort((a,b) => a - b)
+ }
