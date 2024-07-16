572d898bf1ac76d1b18aeb5956d355db765115ff
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-07-16 13:08:13 -0400

    added x's and o's

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1227,3 +1227,36 @@ function solution(nums){
    
    return nums.sort((a,b) => a - b)
  }
+
+//  Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+// Examples input/output:
+
+// XO("ooxx") => true
+// XO("xooxx") => false
+// XO("ooxXm") => true
+// XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+// XO("zzoo") => false
+
+function XO(str) {
+  //code here
+
+const newStr = str.toLowerCase()
+let xCount = 0;
+let oCount = 0;
+
+newStr.split("").forEach((el) => {
+  if(el === "x") {
+    xCount ++
+  } else if (el === "o") {
+    oCount ++
+  }
+})
+
+if(xCount === oCount) {
+  return true
+} else {
+  return false
+}
+
+}
