fc2d4d92936050a0b2b1560c738b301c2a110dbc
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-05 11:19:22 -0400

    added 8kyu

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1409,3 +1409,39 @@ function howManyLightsabersDoYouOwn(name) {
     return 0
   }
 }
+
+
+// write me a function stringy that takes a size and returns a string of alternating 1s and 0s.
+
+// the string should start with a 1.
+
+// a string with size 6 should return :'101010'.
+
+// with size 4 should return : '1010'.
+
+// with size 12 should return : '101010101010'.
+
+// The size will always be positive and will only use whole numbers.
+// use while loop to  accomplish this if number is odd print a 1 if even print a 0
+
+function stringy(size) {
+  // your code here
+  let i = 0
+  let str = ""
+
+while (i < size) {
+  console.log(i)
+  if(i % 2 === 0) {
+  str += "1"
+  } else {
+    str += "0"
+  }
+    // console.log(i);  // Output the value of i
+  i++;     
+  
+  // Increment i to avoid an infinite loop
+}
+ return str
+}
+
+console.log(stringy(4))
