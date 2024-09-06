720a06371a476696c50b7514fd787333c123dc74
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-06 19:09:27 -0400

    added non repeating 5kyu

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1437,6 +1437,7 @@ while (i < size) {
     str += "0"
   }
     // console.log(i);  // Output the value of i
+    
   i++;     
   
   // Increment i to avoid an infinite loop
@@ -1445,3 +1446,27 @@ while (i < size) {
 }
 
 console.log(stringy(4))
+
+// Write a function named first_non_repeating_letter† that takes a string input, and returns the first character that is not repeated anywhere in the string.
+
+// For example, if given the input 'stress', the function should return 't', since the letter t only occurs once in the string, and occurs first in the string.
+
+// As an added challenge, upper- and lowercase letters are considered the same character, but the function should return the correct case for the initial letter. For example, the input 'sTreSS' should return 'T'.
+
+// If a string contains all repeating characters, it should return an empty string ("");
+
+// † Note: the function is called firstNonRepeatingLetter for historical reasons, but your function should handle any Unicode character.
+
+function firstNonRepeatingLetter(s) {
+  // Add your code here
+  for (let i = 0; i < s.length; i++) {
+        let char = s[i];
+        // Check if the first and last occurrence of the character are the same
+        if (s.indexOf(char) == s.lastIndexOf(char)) {
+            return char;  // Return the first repeating character
+        }
+    }
+    return "";  // Return null if no repeating character is found
+}
+
+console.log(firstNonRepeatingLetter("stress"))
