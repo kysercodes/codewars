a100f00ff93ed6cf6d125c52dfdafcf7a450e4d8
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-12 08:49:36 -0400

    added 8kyu

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1519,3 +1519,25 @@ function reverseWords(str){
   }
   
   console.log(reverseWords("yoda doesn't speak like this"))
+
+  // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
+
+// Hint for R users:
+
+// The length of string is not always the same as the number of characters
+// For example: (Input1, Input2) --> output
+
+// ("1", "22") --> "1221"
+
+function solution(a, b){
+  // your code here
+  if(a.length < b.length) {
+    console.log(a)
+    return `${a}${b}${a}`
+  } else {
+     return `${b}${a}${b}`
+  }
+  
+}
+
+console.log(solution('U', 'False'))
