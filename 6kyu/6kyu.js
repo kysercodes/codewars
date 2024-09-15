c756773d9a1c40eba9a19ef39e9d6c0d6726ef2d
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-15 11:46:49 -0400

    7kyu split the string

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1541,3 +1541,30 @@ function solution(a, b){
 }
 
 console.log(solution('U', 'False'))
+
+// Filter the number
+// Oh, no! The number has been mixed up with the text. Your goal is to retrieve the number from the text, can you return the number back to its original state?
+
+// Task
+// Your task is to return a number from a string.
+
+// Details
+// You will be given a string of numbers and letters mixed up, you have to return all the numbers in that string in the order they occur.
+
+var filterString = function(value) {
+  //Complete this function :) 
+  
+  const newArr = []
+  
+ value.split("").forEach((el) => {
+   if(!isNaN(el)) {
+     newArr.push(el)
+   } 
+
+ })
+  
+  return newArr.join("")
+}
+
+console.log(filterString('145abs'))
+
