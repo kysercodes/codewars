11e15b0adc6948a67c03cd3f96d008da510179aa
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-18 13:38:04 -0400

    added 7kyu solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1520,6 +1520,26 @@ function reverseWords(str){
   
   console.log(reverseWords("yoda doesn't speak like this"))
 
+
+//   Complete the solution so that it takes the object (JavaScript/CoffeeScript) or hash (ruby) passed in and generates a human readable string from its key/value pairs.
+
+// The format should be "KEY = VALUE". Each key/value pair should be separated by a comma except for the last pair.
+
+// Example:
+
+// solution({a: 1, b: '2'}) // should return "a = 1,b = 2"
+
+function solution(pairs){
+  const newArr = []
+  // TODO: complete
+  for (const [key, value] of Object.entries(pairs)) {
+  newArr.push(`${key} = ${value}`);
+}
+  return newArr.join()
+}
+
+console.log(solution({'a': 1, 'b': 2}))
+
   // Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty ( zero length ).
 
 // Hint for R users:
