ad688c9433528f94dda91e350ea9f7255c8d4266
Antoine <65553139+kysercodes@users.noreply.github.com>
2024-09-19 11:49:20 -0400

    added solution

--- 6kyu/6kyu.js
+++ 6kyu/6kyu.js
@@ -1637,3 +1637,43 @@ function spacify(str) {
 }
 
 console.log(spacify('hello world'))
+
+// Complete the function which takes a non-zero integer as its argument.
+
+// If the integer is divisible by 3, return the string "Java".
+
+// If the integer is divisible by 3 and divisible by 4, return the string "Coffee"
+
+// If one of the condition above is true and the integer is even, add "Script" to the end of the string.
+
+// If none of the condition is true, return the string "mocha_missing!"
+
+// Examples
+// 1   -->  "mocha_missing!"
+// 3   -->  "Java"
+// 6   -->  "JavaScript"
+// 12  -->  "CoffeeScript"
+function checkInteger(num) {
+  let result = "";
+
+  if (num % 3 === 0 && num % 4 === 0) {
+    result = "Coffee";
+  } else if (num % 3 === 0) {
+    result = "Java";
+  } else {
+    result = "mocha_missing!";
+  }
+
+  if ((num % 3 === 0 || (num % 3 === 0 && num % 4 === 0)) && num % 2 === 0) {
+    result += "Script";
+  }
+
+  return result;
+}
+
+// Examples
+console.log(checkInteger(1));   // "mocha_missing!"
+console.log(checkInteger(3));   // "Java"
+console.log(checkInteger(6));   // "JavaScript"
+console.log(checkInteger(12));  // "CoffeeScript"
+
